922b030645ca8a2a0dbbe6dc6590bd971c51e292
slinder8 <sjtlinder@icloud.com>
2025-09-07 16:10:44 -0400

    Updated tech stack and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ function About() {
         <div id="about" className={"section"}>
             <h1 align={"center"}>About</h1>
             <div className="row">
-                <div className="tbcontainer col-6 w-40" style={{paddingTop: "3%", paddingLeft: "5%"}}>
+                <div className="tbcontainer col-7 w-40" style={{paddingTop: "3%", paddingLeft: "5%"}}>
                 <span>
-                    My name is Stephen, and I am a computer science and math student at Georgia Tech. My primary interests are in back-end development, algorithms, and low-latency computing. As a result, you might not be surprised that my favorite language is C++! I also have some practical experience in web development, primarily using Django and React. Outside of computer science,
+                    My name is Stephen, and I am a computer science and math student at Georgia Tech. My primary interests are in back-end development, algorithms, and low-latency computing. As a result, you might not be surprised that my favorite language is C++! I also have industry experience in web development with FanDuel, primarily using React front-end and Spring Boot back-end. Outside of computer science,
                     I enjoy playing chess, playing violin in the Georgia Tech Symphony Orchestra, and lifting weights. If you want to work on a project together or connect for any other reason, please reach out!
                 </span>
                 <TechStack />
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -5,26 +5,32 @@ const techList = [
     { name: 'C', logo: '/assets/C.png' },
     { name: 'Python', logo: '/assets/Python.png' },
     { name: 'Java', logo: '/assets/Java.png' },
+    { name: 'Typescript', logo: '/assets/Typescript.svg' },
     { name: 'Javascript', logo: '/assets/JavaScript.png' },
     { name: 'Docker', logo: '/assets/Docker.png' },
     { name: 'React', logo: '/assets/React.png' },
+    { name: 'Spring Boot', logo: '/assets/SpringBoot.svg' },
+    { name: 'AWS', logo: '/assets/AWS.png' },
+    { name: 'Datadog', logo: '/assets/Datadog.png' },
     { name: 'VS Code', logo: '/assets/VSCode.png' },
 ];
 
 const TechStack = () => {
     return (
         <div className="tech-stack-container">
-            <h3>My Tech Stack</h3>
-            <ol className="tech-list">
-                {techList.map((tech, index) => (
-                    <li className="tech-item" key={index}>
-                        <img src={tech.logo} alt={tech.name} />
-                        <span>{tech.name}</span>
-                    </li>
-                ))}
-            </ol>
+            <div className="tech-stack-inner">
+                <h3>My Tech Stack</h3>
+                <ol className="tech-list">
+                    {techList.map((tech, index) => (
+                        <li className="tech-item" key={index}>
+                            <img src={tech.logo} alt={tech.name}/>
+                            <span>{tech.name}</span>
+                        </li>
+                    ))}
+                </ol>
+            </div>
         </div>
-    );
-};
+            );
+            };
 
-export default TechStack;
+            export default TechStack;
